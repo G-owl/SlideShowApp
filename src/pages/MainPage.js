39b782efd3d8a3f8833b5aec79c32c1b1ab3d61e
G-owl <rockraj456@gmail.com>
2020-11-22 18:09:57 +0530

    Added Sound

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import MainImage from '../assets/2-nature.jpg';
+import MainBGM from '../assets/rts.mp3';
 import './MainPage.styles.css';
+var Sound = require('react-sound').default;
 
 const MainPage = ({ onNext }) => {
     return ( 
         <div className="main-page">
+            <Sound
+      url={MainBGM}
+      playStatus={Sound.status.PLAYING}
+      volume={10}
+    />
             <img src={MainImage} className="main-img" style={{ height: '98vh'}} />
             <i className="next-btn fas fa-arrow-right" onClick={onNext} />
         </div>
