9dde90a8434e7009207fb716c5b84fbf1715413c
G-owl <rockraj456@gmail.com>
2020-11-23 19:45:59 +0530

    ggwp

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import MainImage from '../assets/2-nature.jpg';
+import MainImage from '../assets/d02.jpg';
 import MainBGM from '../assets/rts.mp3';
 import './MainPage.styles.css';
 var Sound = require('react-sound').default;
--- src/pages/SlideShow.js
+++ src/pages/SlideShow.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import SlideShowImg from '../assets/2-nature.jpg';
+import SlideShowImg from '../assets/d1.jpg';
+import SlideShowImg1 from '../assets/d2.png';
+import SlideShowImg2 from '../assets/d3.png';
+import SlideShowImg3 from '../assets/d4.png';
+import SlideShowImg4 from '../assets/d5.png';
 import { Slide } from 'react-slideshow-image';
 import "./SlideShow.styles.css";
 import 'react-slideshow-image/dist/styles.css'
 
 const images = [
-    SlideShowImg, SlideShowImg, SlideShowImg
+    SlideShowImg, SlideShowImg1, SlideShowImg2, SlideShowImg3, SlideShowImg4
 ];
 
 const SlideShow = () => {
@@ -22,6 +26,14 @@ const SlideShow = () => {
                 </div>
                 <div className="each-slide">
                     <div style={{'backgroundImage': `url(${images[2]})`}}>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div style={{'backgroundImage': `url(${images[3]})`}}>
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div style={{'backgroundImage': `url(${images[4]})`}}>
                     </div>
                 </div>
             </Slide> 
